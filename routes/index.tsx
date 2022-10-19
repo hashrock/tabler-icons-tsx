25031ddd64343fead39d30445e40508bd43b5d1d
hashrock <hashrock@users.noreply.github.com>
2022-10-19 22:00:49 +0900

    add instruction

--- routes/index.tsx
+++ routes/index.tsx
@@ -30,6 +30,29 @@ export default function Home({ data }: PageProps<Icons>) {
         </link>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        <h1 class="text-6xl">TSX Tabler Icons</h1>
+
+        <p class="mt-2">
+          Easy to use{" "}
+          <a
+            class="text-blue-500 hover:underline"
+            href="https://tabler-icons.io/"
+          >
+            Tabler Icons
+          </a>{" "}
+          in your Fresh app. Just import the icons.
+        </p>
+        <p class="mt-6">
+          <a href="https://fresh.deno.dev">
+            <img
+              width="197"
+              height="37"
+              src="https://fresh.deno.dev/fresh-badge.svg"
+              alt="Made with Fresh"
+            />
+          </a>
+        </p>
+
         <IconList icons={data.icons} />
       </div>
     </>

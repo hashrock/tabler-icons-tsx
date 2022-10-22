8494495b5e2d87932900498e389e941438f3aa47
hashrock <hashrock@users.noreply.github.com>
2022-10-22 21:28:50 +0900

    fix: update converter

--- convert.ts
+++ convert.ts
@@ -1,17 +1,23 @@
 const files = Deno.readDirSync("react");
 
+// write files to icons.json
+const icons = [];
+
 for (const file of files) {
   const name = file.name;
   const content = Deno.readTextFileSync(`react/${name}`);
   const result = replace(content);
   Deno.mkdirSync("tsx", { recursive: true });
   Deno.writeTextFileSync(`tsx/${name.replace(/\.js$/, ".tsx")}`, result);
+  icons.push(name.replace(/\.js$/, ""));
 }
+
+Deno.writeTextFileSync("data/icons.json", JSON.stringify(icons, null, 2));
 
 function replace(content: string) {
   const patterns = [
     {
-      match: 'import * as React from "react";',
+      match: /import \* as React from "react";\n\n/mg,
       replace: "",
     },
     {

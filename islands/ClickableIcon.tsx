3445405bdee9ab54543653b330c2b0e4c5cfa771
hashrock <hashrock@users.noreply.github.com>
2022-10-22 22:38:14 +0900

    make copy button focusable

--- islands/ClickableIcon.tsx
+++ islands/ClickableIcon.tsx
@@ -30,7 +30,7 @@ export default function ClickableIcon(props: ClickableIconProps) {
       <i class={`ti ti-${icon} text-2xl`}></i>
       <div class="text-gray-500 flex-1">{icon}</div>
       <button
-        class={"px-2 py-1 opacity-0 group-hover:opacity-100 " +
+        class={"px-2 py-1 opacity-0 group-hover:opacity-100 focus:opacity-100 " +
           (copied ? "text-green-500" : "text-gray-500")}
         onClick={() => {
           copy;

42d78832df03d839ed19c6cce37aaf5b23c5fbf5
hashrock <hashrock@users.noreply.github.com>
2022-12-13 01:41:04 +0900

    chore: remove extra div

--- islands/CodeBlock.tsx
+++ islands/CodeBlock.tsx
@@ -21,22 +21,20 @@ export default function CodeBlock(props: CodeBlockProps) {
 
   const [copied, setCopied] = useState(false);
   return (
-    <div class="">
-      <div class="relative">
-        <pre class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap">
-          <code
-            dangerouslySetInnerHTML={{ __html: highlighted }}
-          />
-        </pre>
-        {props.copy && (
-          <button
-            onClick={onCopy}
-            class="absolute top-2 right-2 px-3 py-2 border border-gray-400 rounded text-white bg-gray-800"
-          >
-            {copied ? "Copied!" : "Copy"}
-          </button>
-        )}
-      </div>
+    <div class="relative">
+      <pre class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap">
+        <code
+          dangerouslySetInnerHTML={{ __html: highlighted }}
+        />
+      </pre>
+      {props.copy && (
+        <button
+          onClick={onCopy}
+          class="absolute top-2 right-2 px-3 py-2 border border-gray-400 rounded text-white bg-gray-800"
+        >
+          {copied ? "Copied!" : "Copy"}
+        </button>
+      )}
     </div>
   );
 }

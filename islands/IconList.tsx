7b90e378c540ff53f4d9ca5ab9083945d6ca0dc0
hashrock <hashrock@users.noreply.github.com>
2023-09-28 15:32:04 +0900

    chore: auto display example

--- islands/IconList.tsx
+++ islands/IconList.tsx
@@ -1,3 +1,5 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Button } from "../components/Button.tsx";
 import ClickableIcon from "./ClickableIcon.tsx";
 import { useState } from "preact/hooks";
 interface IconListProps {
@@ -13,16 +15,25 @@ export default function IconList(props: IconListProps) {
 
   return (
     <>
-      <div class="mt-8">
+      <div class="mt-8 flex">
         <input
+          disabled={!IS_BROWSER}
           class="border border-gray-300 rounded p-2 w-full"
           type="text"
           placeholder="Search"
           autofocus
+          value={search}
           onInput={(e) => {
             setSearch((e.target as HTMLInputElement).value);
           }}
         />
+        <Button
+          onClick={() => {
+            setSearch("");
+          }}
+        >
+          Clear
+        </Button>
       </div>
       <ul class="mt-8 grid grid-cols-4">
         {filteredIcons.map((icon) => <ClickableIcon name={icon} />)}
